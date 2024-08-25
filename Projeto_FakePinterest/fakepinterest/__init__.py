1779b27c1a40ef782021d1e2aedcb7c37223785f
HPoirot22 <nascimentovictoria02@gmail.com>
2024-08-25 12:00:05 -0300

    ajuste __init__.py

--- Projeto_FakePinterest/fakepinterest/__init__.py
+++ Projeto_FakePinterest/fakepinterest/__init__.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SECRET_KEY"] = "5b553bb1bf60247c05ca750699514e0e"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config['UPLOAD_FOLDER'] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
